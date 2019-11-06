0a94de85af613c0c2c5b2a562a29c76d3231cc67
Michael Darrah <webmaster@xgeneration.net>
2019-11-06 06:15:32 -0500

    JS error fix & contact link fix

--- js/activation.js
+++ js/activation.js
@@ -194,53 +194,6 @@
         $('.venobox')
             .venobox();
 
-        /*==============================================================*/
-        // Load More Function Start
-        /*==============================================================*/
-        ! function(e) {
-            "use strict";
-            e(document).ready(function() {
-                function t(t) {
-                    o.find(".hidden").removeClass("hidden");
-                    var a = r.filteredItems.slice(t, r.filteredItems.length).map(function(e) {
-                        return e.element
-                    });
-                    e(a).addClass("hidden"),
-                        o.isotope("layout"),
-                        0 == a.length ? jQuery("#loadMore").hide() : jQuery("#loadMore").show()
-                }
-                var o = e(".pro-all-projects").isotope({
-                        itemSelector: ".pitem",
-                        layoutMode: "fitRows",
-                        getSortData: {
-                            name: ".name",
-                            symbol: ".symbol",
-                            number: ".number parseInt",
-                            category: "[data-category]",
-                            weight: function(t) {
-                                var o = e(t).find(".weight").text();
-                                return parseFloat(o.replace(/[\(\)]/g, ""))
-                            }
-                        }
-                    }),
-                    a = 6,
-                    i = 3,
-                    n = a,
-                    r = o.data("isotope");
-                t(a),
-                    e("#loadMore").on("click",
-                        function() {
-                            e("#filters").data("clicked") ? (n = i, e("#filters").data("clicked", !1)) : n = n, n += i, t(n)
-                        }
-                    ),
-                    e("#filters").on("click",
-                        function() {
-                            e(this).data("clicked", !0), t(i)
-                        }
-                    )
-            })
-        }
-        (jQuery);
 
         /*==============================================================*/
         //flickity sliders
